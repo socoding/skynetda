379a4edd1b6cfc5308873dc4cc724250a2938520
xudechao <xdczju@163.com>
2023-05-04 18:52:05 +0800

    fix bat bug for .

--- vscext/bin/debugger.lua
+++ vscext/bin/debugger.lua
@@ -96,12 +96,11 @@ local function parse_file_envs(cwd, env_file, env_prefix)
 
     local winos = os.getenv("vscdbg_platform") == "windows"
 
-    -- if not is_abspath(env_file) then
-    --     env_file = cwd .. (winos and "\\" or "/") .. env_file
-    -- end
-
     local file, errmsg
     if winos then
+        if not is_abspath(env_file) then
+            env_file = cwd .. (winos and "\\" or "/") .. env_file
+        end
         file, errmsg = io.popen("@echo off & cd " .. cwd .. " & call " .. env_file .. " & set", 'r')
     else
         file, errmsg = io.popen("cd " .. cwd .. "; source " .. env_file .. "; export", 'r')
